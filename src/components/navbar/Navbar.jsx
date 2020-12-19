26dbdff19b8e462d1ed66ef9df406dea1b8b133d
ydahal1 <yadhapdahal@yadhaps-Mini.attlocal.net>
2020-12-19 11:41:22 -0500

    done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,7 @@ class Navbar extends Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a className="navbar-brand" href="#">
-          My Portfolio
-        </a>
-        <div className="float-right">v</div>
-      </nav>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark"></nav>
     );
   }
 }
